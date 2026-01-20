85067efbf9b010cac9ad74cd8df8072084bee864
girimugundankumar <girimugundan@vt.edu>
2026-01-20 15:27:12 -0500

    added the integration of vel to pos via measured values for processor

--- functions/process_mrac_observer_log.m
+++ functions/process_mrac_observer_log.m
@@ -366,9 +366,9 @@ function [] = process_mrac_observer_log(flightRunNames,baseDir,controller,proper
                         der.observer.mrao2l.obs_err.vz];
 
         % Compute the integrated position values
-        der.observer.integrated_pos_from_vel.x = ;
-        der.observer.integrated_pos_from_vel.y = ;
-        der.observer.integrated_pos_from_vel.z = ;
+        der.observer.integrated_pos_from_vel.x = cumtrapz(log.Controller_Time_s, log.Velocity_x_ms);
+        der.observer.integrated_pos_from_vel.y = cumtrapz(log.Controller_Time_s, log.Velocity_y_ms);
+        der.observer.integrated_pos_from_vel.z = cumtrapz(log.Controller_Time_s, log.Velocity_z_ms);
 
         % -- pointwise euclidean norms
         e_pos_mrao_norm   = vecnorm(e_pos_mrao,   2, 2);   % ||e_pos(t)|| [web:83][web:84]
